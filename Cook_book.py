df9dc54cae77d86b2690a0c6c226832ed73cef63
Vadim Nikolaev <vadn@yandex.ru>
2024-06-06 20:48:05 +0300

    Cook_book - 3й день мучений

--- Cook_book.py
+++ Cook_book.py
@@ -1,42 +1,61 @@
-f = open('recipes.txt', encoding='utf-8')
-data = f.read()
-f.close()
+# f = open('recipes.txt', encoding='utf-8')
+# data = f.read()
+# f.close()
 
 cook_book = {}
+list_name_temp = []
+dish_name = []
+count = 0
+nn = 0
+
 with open('recipes.txt', encoding='utf-8') as f:
-    count =0
-    nn = 0
     for line in f:
-        count +=1
-        #Считаем длинну строки "l="
+        count += 1
+
+        # Считаем длинну строки "l="
         l = len(line.strip())
         str_name = line.strip()
         print(count, str_name)
-        #print("Длинна строки:",l,"\n")
+        # zipped = zip(count, str_name)
+        # print("Длинна строки:",l,"\n")
 
-        if count == 1:    #Определяем ключ словаря и создаём словарь
-            cook_book[str_name] = []
-            nn +=1
-            list_name_temp = []
-            # print(type(list_name_temp))
-            # list_name = list(list_name_temp)
-            # print(list_name)
+        if count == 1:  # Определяем ключ словаря и добавляем его словарь
+            # cook_book[str_name] = []
+            # print(cook_book)
+            nn += 1
+            print("Блюдо №", nn)
+            dish_name = str_name
+            print("Имя блюда:", dish_name)
 
-        elif count == 2:    #Формируем значеия словаря
+        elif count == 2:  # Формируем значеия словаря
             recip_len = int(str_name)
-        elif recip_len-1 < count > 2:
-            #cook_book[str_name].append() = str_name
-            #pass
-            list_name_temp.append(str_name)
-        cook_book[str_name] = list_name_temp
+        #
+        elif count >= 3 and count < recip_len + 3:
 
-        #Вычисляем пробелы в рецепте
-        if len(line.strip()) == 0:
-            count=0
-print()
-print(cook_book)
+            ingr = str_name.strip().split("|")
+            for i in ingr:
+                i.strip()
+                # print(i)
+                list_name_temp.append(i)
+
+        # Вычисляем пробелы в рецепте
+        elif len(line.strip()) == 0:
+            count = 0
+            print("Имя блюда:", dish_name)
+            cook_book[dish_name] = list_name_temp
+            # print(cook_book)
+            # print(list_name_temp, type(list_name_temp))
+            # print()
+            #list_name_temp.clear()
+            print(cook_book)
+
+    #print(list_name_temp)
+
+print('----------------')
+print(cook_book, type(cook_book))
 print('Размер рецепта:', count, "строк")
-#
-# for l in data:
-#     print(l)
 
+# cook_book = {}
+# list_name_temp = ['Яйцо ', ' 2 ', ' шт', 'Молоко ', ' 100 ', ' мл', 'Помидор ', ' 2 ', ' шт']
+# cook_book["Омлет"] = list_name_temp
+# print(cook_book)
