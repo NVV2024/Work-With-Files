f509dace476f41cdd6b3cdb0b34be8b92ae75e05
Vadim Nikolaev <vadn@yandex.ru>
2024-06-08 02:13:38 +0300

    Cook book. Задача №1. Release

--- Cook_book.py
+++ Cook_book.py
@@ -1,62 +1,57 @@
-# f = open('recipes.txt', encoding='utf-8')
-# data = f.read()
-# f.close()
-from typing import List
+# Задача №1
+#ver. 08.06 2:11
+
+import pprint
 
 cook_book = {}
-list_name_temp: List[str] = []
 dish_name = []
 count = 0
-nn = 0
+count_ingr = 0
 
 with open('recipes.txt', encoding='utf-8') as f:
     for line in f:
         count += 1
-        # Считаем длинну строки "l="
-        l = len(line.strip())
-        str_name = line.strip()
-        print(count, str_name)
-        # zipped = zip(count, str_name)
-        # print("Длинна строки:",l,"\n")
 
-        if count == 1:  # Определяем ключ словаря и добавляем его словарь
-            # cook_book[str_name] = []
-            # print(cook_book)
-            nn += 1
-            print("Блюдо №", nn)
+        # Считаем длинну строки "l="
+        str_name = line.strip()
+        l = len(str_name)
+        #print(count, str_name)
+
+        if count == 1:  # Определяем ключ словаря dish_name
+            # ---- Обнуляем значение, чтобы заново считать новое блюдо
+            list_name_temp = []
+            ingradients_list = []
+            ingredient_dict = {}
+            #----
+
+            #print("Блюдо №", nn)
             dish_name = str_name
             #print("Имя блюда:", dish_name)
 
-        elif count == 2:  # Формируем значеия словаря
+        elif count == 2:  # Запоминаем кол-во инградиентов
             recip_len = int(str_name)
         #
-        elif count >= 3 and count <= recip_len + 2:
-            ingr = str_name.strip().split("|")
-            for i in ingr:
-                i.strip()
-                # print(i)
-                list_name_temp.append(i)
-                #print("Имя блюда:", dish_name)
-                #
-                #print(cook_book.keys(), "\n")
-            #print("Имя блюда:", dish_name)
+        #Формируем список словарей каждого инградиента
+        elif count >= 3 and count < recip_len + 3:
+            count_ingr += 1
+            ingredients = str_name.strip().split("|")
+            ingredients_keys = ['ingredient_name', 'quantity', 'measure']
+            ingredient_dict = dict(zip(ingredients_keys, ingredients))
+            ingradients_list.append(ingredient_dict)
+            if count_ingr == recip_len:
+                count_ingr = 0    #Обнуляем значение, чтобы заново считать новое блюдо
+                cook_book[dish_name] = ingradients_list
 
         # Вычисляем пробелы в рецепте
         elif len(line.strip()) == 0:
-            count = 0
-            cook_book[dish_name] = list_name_temp
-            print("Имя блюда:", dish_name)
-            print(cook_book.items())
+            count = 0    #Обнуляем значение, чтобы заново считать новое блюдо
 
         else:
             pass
 
-
-    #print(list_name_temp)
-
-print('----------------')
-print(cook_book)
-print('Размер рецепта:', count, "строк")
+#print('----------------')
+print(f'cook book = {cook_book}')
+#print('Размер рецепта:', count, "строк")
 
 # cook_book = {}
 # list_name_temp = ['Яйцо ', ' 2 ', ' шт', 'Молоко ', ' 100 ', ' мл', 'Помидор ', ' 2 ', ' шт']
