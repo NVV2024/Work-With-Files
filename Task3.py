28d3c55b994c20a5df6404f5bead2c009bab4c2d
Vadim Nikolaev <vadn@yandex.ru>
2024-06-10 23:43:22 +0300

    Задание №3. 10/06/24 - 23:42 - первоначальная сортировка

--- Task3.py
+++ Task3.py
@@ -2,19 +2,39 @@
 
 #from pathlib import Path
 import os
+from typing import Dict, Any
 
-count = 0
+files = {}
+f_list = []
+sorted_dict = {}
 
-with open('1.txt', encoding='utf-8') as f:
-    text1 = f.read().split(" ")
-    name = f.name
-    for line in f:
-        count += 1
-        #pass
+files_array = ['1.txt','2.txt']
 
-print(name)
-print(count)
-print(text1)
+def qaz(files_array):
+    count = 0
+    #sorted_dict = {}
+    for file in files_array:
+        with open(file, encoding='utf-8') as f:
+            name = f.name
+            list_list = [name]
+            #print(name)
+            text1 = f.readlines()
+            list_list.append(len(text1))
+            files[name] = len(text1)
+    sorted_list = sorted(files.items(), key=lambda x: x[1])
+    sorted_dict = dict(sorted_list)
+    print(sorted_dict)
+    return sorted_dict
+
+
+#print(f_list)
+    #file[name] = count
+
+qaz(files_array)
+
+#print(files)
+#print(sorted_dict)
+
 
 #name = os.path.basename(r'C:\python3\file.txt ')
     #file1 = f.read()
